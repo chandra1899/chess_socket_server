64450801c518f81d72a7166a7404f77770ce29b9
chandra <101719497+chandra1899@users.noreply.github.com>
2023-09-23 18:35:37 +0530

    deleted vercel.json and commented console.logs

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const httpServer = http.createServer();
   
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "https://admin.socket.io/"], // Replace with your frontend URL
+    origin: [process.env.SETDISCONNECT_URL, "https://admin.socket.io/"], // Replace with your frontend URL
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
@@ -22,18 +22,18 @@ const io = new Server(httpServer, {
       socket.email=email;
       socket.roomName=roomName
       await socket.join(roomName);
-      console.log('room and email',roomName,email);
+      // console.log('room and email',roomName,email);
   });
 
   await socket.on("send_msg", async (data) => {
-    console.log(data, "DATA");
+    // console.log(data, "DATA");
     if(data.roomId){
       await io.to(data.roomId).emit("receive_msg", data);
     }
   });
 
   await socket.on("move", async (data) => {
-    console.log("data",data);
+    // console.log("data",data);
    
     if(data.roomId){
       await io.to(data.roomId).emit("moved",data);
@@ -41,17 +41,17 @@ const io = new Server(httpServer, {
   });
 
   await socket.on("receive_draw_req", async (email,roomName) => {
-    console.log("email",email);
+    // console.log("email",email);
       await io.to(roomName).emit("receive_draw_req",email);    
   });
 
   await socket.on("game_over", async (roomName,email) => {
-    console.log("email",email);
+    // console.log("email",email);
       await io.to(roomName).emit("game_over",email);  
   });
 
   await socket.on("draw_accepted", async (email,roomName) => {
-    console.log("email",email);
+    // console.log("email",email);
       await io.to(roomName).emit("draw_accepted",email);    
   });
 
