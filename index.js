2dd22abef4d5c5c1e73a372b352658461ff00e0e
chandra <101719497+chandra1899@users.noreply.github.com>
2024-06-30 00:07:40 +0530

    removed unwanted code

--- index.js
+++ index.js
@@ -8,69 +8,69 @@ const httpServer = http.createServer();
   
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000","https://chessmastershub.vercel.app", "https://admin.socket.io"], // Replace with your frontend URL
+    origin: ["http://localhost:3000","https://chessmastershub.vercel.app", "https://admin.socket.io", "http://localhost:8000"], // Replace with your frontend URL
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
   },
 });
 
-    io.on("connection",async  (socket) => {
-    console.log("A user connected:", socket.id);
-    await socket.on('joinRoom', async (roomName,email) => {
+io.on("connection",async  (socket) => {
+    // console.log("A user connected:", socket.id);
+
+    socket.on('establish-conection',async (roomName,email)=>{
+      // join room 
       if(!email) return ;
       socket.email=email;
       socket.roomName=roomName
-      await socket.join(roomName);
-      console.log('room and email',roomName,email);
-  });
+      socket.join(roomName);
+      // console.log('room and email',roomName,email);
 
-  await socket.on("send_msg", async (data) => {
-    console.log("send_msg");
-      await io.to(data.roomId).emit("receive_msg", data);
-  });
+      socket.emit("connection-established",{})
 
-  await socket.on("move", async (data) => {
-    console.log("move");
-   
-      await io.to(data.roomId).emit("moved",data);
-  });
+      socket.on("send_msg", async (data) => {
+          io.in(data.roomId).emit("receive_msg", data);
+      });
+      socket.on("move", async (data) => {
+        io.in(data.roomId).emit("moved",data);
+      });
 
-  await socket.on("receive_draw_req", async (email,roomName) => {
-    console.log("receive_draw_req");
-      await io.to(roomName).emit("receive_draw_req",email);    
-  });
+      socket.on("receive_draw_req", async (email,roomName) => {
+        io.in(roomName).emit("receive_draw_req",email);    
+      });
 
-  await socket.on("game_over", async (roomName,email) => {
-    console.log("game_over");
-      await io.to(roomName).emit("game_over",email);  
-  });
+      socket.on("game_over", async (roomName,email) => {
+          io.in(roomName).emit("game_over",email);  
+      });
 
-  await socket.on("draw_accepted", async (email,roomName) => {
-    console.log("draw_accepted");
-      await io.to(roomName).emit("draw_accepted",email);    
-  });
+      socket.on("draw_accepted", async (email,roomName) => {
+          io.in(roomName).emit("draw_accepted",email);    
+      });
 
-  socket.on('disconnect', async function () {
-    let res=await fetch(`https://chessmastershub.vercel.app/api/setdisconnected`,{
-      method:'POST',
-      headers:{
-        'Access-Control-Allow-Origin': '*',
-        Accept:"application/json",
-        "Content-Type":"application/json"
-      },
-      credentials:'include',
-      body:JSON.stringify({
-        email:socket.email,
-        roomName:socket.roomName
-      })
+      socket.on('disconnect', async function () {
+        socket.leave(roomName)
+        console.log(email, 'user left room', roomName, 'with socket id', socket.id);
+        let res=await fetch(`https://chessmastershub.vercel.app/api/setdisconnected`,{
+          method:'POST',
+          headers:{
+            'Access-Control-Allow-Origin': '*',
+            Accept:"application/json",
+            "Content-Type":"application/json"
+          },
+          credentials:'include',
+          body:JSON.stringify({
+            email:socket.email,
+            roomName:socket.roomName
+          })
+        })
+        if(res.status===200){
+          console.log(`${socket.email} is disconnected from ${socket.roomName}`);
+        }
+      });
+
     })
-    if(res.status===200){
-      console.log(`${socket.email} is disconnected from ${socket.roomName}`);
-    }
-  });
 });
 
-httpServer.listen(process.env.PORT || 3001, () => {
-  console.log(`Socket.io server is running on port ${process.env.PORT || 3001}`);
+httpServer.listen(3001, () => {
+  console.log(`Socket.io server is running on port ${3001}`);
 });
